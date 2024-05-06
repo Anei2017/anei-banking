4d6b32eb5f41b1214af041513d6a3d77798853ac
Patrick Anei <patrick_anei@hotmail.com>
2024-05-06 21:35:45 +1000

    second commit

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TotalBalanceBox = ({
+    accounts =[], totalBanks, totalCurrentBalance
+
+}: TotalBalanceBoxProps) => {
+    return (
+      <section className='total-balance'>
+    <div></div>
+    </section>
+  )
+}
+
+export default TotalBalanceBox
