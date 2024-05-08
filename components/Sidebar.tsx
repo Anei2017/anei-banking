98c54c8d4e5d08be9725f602a5ffe5b585b7e13a
Patrick Anei <patrick_anei@hotmail.com>
2024-05-08 23:01:30 +1000

    another commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-const  Sidebar = ({ user }: SiderbarProps) => {
+const Sidebar = ({ user }: SiderbarProps) => {
   return (
-      <section className='sidebar'> 
-          
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon logo"
+            className="size-[24px]
+                      max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Aneirizon</h1>
+        </Link>
+      </nav>
     </section>
-  )
-}
+  );
+};
 
-export default  Sidebar
+export default Sidebar;
