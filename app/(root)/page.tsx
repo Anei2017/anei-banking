acdb80ae03e2a060f4a96c0a0c5cb15d02feeae5
Patrick Anei <patrick_anei@hotmail.com>
2024-05-09 00:58:03 +1000

    cont-commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Patrick" };
+  const loggedIn = { firstName: 'Patrick' };
   return (
     <section className="home">
       <div className="home-content">
@@ -22,7 +23,13 @@ const Home = () => {
           />
 
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={[]}
+        transactions={[]}
+        banks={[]}
+      />
     </section>
   );
 };
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 "use client"
+=======
+"use client";
+>>>>>>> 6090baa (cont-commit)
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
@@ -7,11 +11,15 @@ import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 
 const Sidebar = ({ user }: SiderbarProps) => {
+<<<<<<< HEAD
     const pathname = usePathname();
+=======
+  const pathname = usePathname();
+>>>>>>> 6090baa (cont-commit)
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
-        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
           <Image
             src="/icons/logo.svg"
             width={34}
@@ -21,6 +29,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
                       max-xl:size-14"
           />
           <h1 className="sidebar-logo">Aneirizon</h1>
+<<<<<<< HEAD
               </Link>
               {sidebarLinks.map((item) =>
               {
@@ -46,7 +55,36 @@ const Sidebar = ({ user }: SiderbarProps) => {
                 )
               })}
                 
+=======
+        </Link>
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    "brightness-[3] invert-0": isActive,
+                  })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })} USER
+>>>>>>> 6090baa (cont-commit)
       </nav>
+      FOOTER
     </section>
   );
 };
